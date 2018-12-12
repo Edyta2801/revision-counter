04ea808979e005b1cd4f7c98d88f320993475c75
Edyta Szarowska <edyta.szarowska@interia.pl>
2018-12-12 22:27:48 +0100

    Router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import Counter from './Counter'
 import './App.css';
+import { BrowserRouter as Router, Route} from 'react-router-dom'
 import PassingProps from './PassingProps/PassingProps'
 
 
 const App = (props) => (
 
   <div>
-    <Counter
+    <Router>
+<Route path='/'exact component={Counter}/>
+<Route path='/counter' component={Counter}/>
+<Route path='/passing-props' component={PassingProps}/>
+
+    {/* <Counter
     startValue={3}/>
     <PassingProps
     passingValue={5}
     passingFun={()=>alert('bu!')}
-    />
+    /> */}
+   </Router> 
   </div>
 )
 
