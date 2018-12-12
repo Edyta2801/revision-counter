f1f504251a390acda043f3f449cc3d968da57ef9
Edyta Szarowska <edyta.szarowska@interia.pl>
2018-12-12 21:25:00 +0100

    create components in folder PassingProps

--- src/PassingProps/PassingProps.js
+++ src/PassingProps/PassingProps.js
@@ -4,6 +4,8 @@ class PassingProps extends React.Component{
     render(){
         return(
             <div>
+                passingValue={this.props.passingValue}
+                passingFun={this.props.passingFun}
 
             </div>
         )
---
+++ src/PassingProps/PassingPropsPrim.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import PassingPropsBis from '.PassingPropsBis'
+
+
+class PassingPropsPrim extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <PassingPropsBis/>
+            </div>)
+    }
+}
+
+export default PassingPropsPrim
