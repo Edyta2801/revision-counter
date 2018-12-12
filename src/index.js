76e96306ce45117b2a12b3f227441e844572e098
Edyta Szarowska <edyta.szarowska@interia.pl>
2018-12-12 23:31:42 +0100

    Navigation

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+
 ReactDOM.render(
-  <App />,
-  document.getElementById('root'))
+  <MuiThemeProvider>
+    <App />
+  </MuiThemeProvider>,
+  document.getElementById('root')
+)
